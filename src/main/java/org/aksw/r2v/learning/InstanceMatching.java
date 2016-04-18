8fc7aecc9c0dbd5f1f35145c0369dab49a537793
Tommaso Soru <mommi84@gmail.com>
2016-04-18 19:39:19 +0200

    minor changes

--- src/main/java/org/aksw/r2v/learning/InstanceMatching.java
+++ src/main/java/org/aksw/r2v/learning/InstanceMatching.java
@@ -22,39 +22,167 @@ public class InstanceMatching {
 //		run(3);
 		// TODO compute median();
 //		DoubleMatrix Mmed = median();
-		wekaInput(10);
+//		wekaInput(50, 90);
 		
+//		wekaInputTest(50, 90, 100);
+//		wekaInputTrain(50, 90, 100, 9);
+
+//		wekaInputTest(10, 10, 100);
+		wekaInputTrain(10, 10, 100, 100);
+
 	}
 	
+	@SuppressWarnings("unused")
 	private static DoubleMatrix median() throws IOException {
 		DoubleMatrix Mmed = DoubleMatrix.loadCSVFile("etc/learning/M_median.csv");
 		System.out.println(Mmed);
 		return Mmed;
 	}
 
-	public static void wekaInput(int dim) throws IOException {
+	public static void wekaInputTest(int dim1, int dim2, final int ROWS) throws IOException {
 		
 		// load matrices
-		DoubleMatrix c1 = loadMatrix("person11-D10", dim);
-		DoubleMatrix c2 = loadMatrix("person12-D10", dim);
+		DoubleMatrix c1 = loadMatrix("person11-D"+dim1, dim1);
+		DoubleMatrix c2 = loadMatrix("person12-D"+dim2, dim2);
 		
 		// load labels
-		ArrayList<String> l1 = loadLabels("person11-D10");
-		ArrayList<String> l2 = loadLabels("person12-D10");
-				
+		ArrayList<String> l1 = loadLabels("person11-D"+dim1);
+		ArrayList<String> l2 = loadLabels("person12-D"+dim2);
+		
 		// index vectors from dataset 2
 		HashMap<String, DoubleMatrix> c2index = new HashMap<>();
 		for(int i=0; i<l2.size(); i++)
 			c2index.put(l2.get(i), c2.getRow(i));
 		
-		String file = "etc/learning/weka_"+(int)(Math.random()*10000)+".csv";
+		String file = "etc/learning/weka_test_set_"+dim1+"_"+dim2+".csv";
 		PrintWriter pw = new PrintWriter(new File(file));
-		for(int i=0; i < dim; i++) {
-			if(i<5)
-				pw.print("p1c"+i+",");
-			else
-				pw.print("p2c"+(i-5)+",");
+		
+		// print headers
+		for(int i=0; i < dim1; i++)
+			pw.print("p1c"+i+",");
+		for(int i=dim1; i < dim1+dim2; i++)
+			pw.print("p2c"+(i-dim1)+",");
+		pw.println("label");
+		
+		// print body
+		int r = 0;
+		for(int i=0; r < ROWS; i++) {
+			
+			String p1 = l1.get(i);
+			String p2 = sameEntityOf(p1);
+			DoubleMatrix v2 = c2index.get(p2);
+			if(v2 == null) // no sameas here...
+				continue;
+			
+			DoubleMatrix v1 = c1.getRow(i);
+			r++;
+			
+			pw.print(v1.toString().substring(1, v1.toString().length() - 1).replaceAll(";", ",") + ", ");
+			pw.print(v2.toString().substring(1, v2.toString().length() - 1).replaceAll(";", ",") + ", ");
+			pw.println("POS");
+			
+			// create all negative examples
+			for(int j=0; j<c2.rows; j++) {
+				if(j != i) {
+					DoubleMatrix v2n = c2.getRow(j);
+					pw.print(v1.toString().substring(1, v1.toString().length() - 1).replaceAll(";", ",") + ", ");
+					pw.print(v2n.toString().substring(1, v2n.toString().length() - 1).replaceAll(";", ",") + ", ");
+					pw.println("NEG");
+				}
+			}
+			
 		}
+		
+		pw.close();
+		System.out.println("Output file: "+file);
+
+
+	}
+
+	public static void wekaInputTrain(int dim1, int dim2, final int ROWS, final int NEG) throws IOException {
+		
+		// load matrices
+		DoubleMatrix c1 = loadMatrix("person11-D"+dim1, dim1);
+		DoubleMatrix c2 = loadMatrix("person12-D"+dim2, dim2);
+		
+		// load labels
+		ArrayList<String> l1 = loadLabels("person11-D"+dim1);
+		ArrayList<String> l2 = loadLabels("person12-D"+dim2);
+		
+		// index vectors from dataset 2
+		HashMap<String, DoubleMatrix> c2index = new HashMap<>();
+		for(int i=0; i<l2.size(); i++)
+			c2index.put(l2.get(i), c2.getRow(i));
+		
+		int id = (int)(Math.random()*10000);
+		String file = "etc/learning/weka_training_set_"+dim1+"_"+dim2+"_"+id+".csv";
+		PrintWriter pw = new PrintWriter(new File(file));
+		
+		// print headers
+		for(int i=0; i < dim1; i++)
+			pw.print("p1c"+i+",");
+		for(int i=dim1; i < dim1+dim2; i++)
+			pw.print("p2c"+(i-dim1)+",");
+		pw.println("label");
+		
+		// print body
+		for(int i=ROWS; i < c1.rows; i++) {
+			
+			String p1 = l1.get(i);
+			String p2 = sameEntityOf(p1);
+			DoubleMatrix v2 = c2index.get(p2);
+			if(v2 == null) // no sameas here...
+				continue;
+			
+			DoubleMatrix v1 = c1.getRow(i);
+			
+			pw.print(v1.toString().substring(1, v1.toString().length() - 1).replaceAll(";", ",") + ", ");
+			pw.print(v2.toString().substring(1, v2.toString().length() - 1).replaceAll(";", ",") + ", ");
+			pw.println("POS");
+			
+			// create k negative examples
+			for(int k=0; k<NEG; k++) {
+				int j;
+				do {
+					j = (int) (Math.random() * c1.rows);
+				} while(i == j);
+				DoubleMatrix v1n = c1.getRow(j);
+				pw.print(v1n.toString().substring(1, v1n.toString().length() - 1).replaceAll(";", ",") + ", ");
+				pw.print(v2.toString().substring(1, v2.toString().length() - 1).replaceAll(";", ",") + ", ");
+				pw.println("NEG");
+			}
+			
+		}
+		
+		pw.close();
+		System.out.println("Output file: "+file);
+
+
+	}
+
+	public static void wekaInput(int dim1, int dim2) throws IOException {
+		
+		// load matrices
+		DoubleMatrix c1 = loadMatrix("person11-D"+dim1, dim1);
+		DoubleMatrix c2 = loadMatrix("person12-D"+dim2, dim2);
+		
+		// load labels
+		ArrayList<String> l1 = loadLabels("person11-D"+dim1);
+		ArrayList<String> l2 = loadLabels("person12-D"+dim2);
+		
+		// index vectors from dataset 2
+		HashMap<String, DoubleMatrix> c2index = new HashMap<>();
+		for(int i=0; i<l2.size(); i++)
+			c2index.put(l2.get(i), c2.getRow(i));
+		
+		int id = (int)(Math.random()*10000);
+		String file = "etc/learning/weka_training_set_"+id+".csv";
+		PrintWriter pw = new PrintWriter(new File(file));
+		for(int i=0; i < dim1; i++)
+			pw.print("p1c"+i+",");
+		for(int i=dim1; i < dim1+dim2; i++)
+			pw.print("p2c"+(i-dim1)+",");
+		
 		pw.println("label");
 		
 		for(int i=0; i < c1.rows; i++) {
@@ -68,8 +196,8 @@ public class InstanceMatching {
 			
 			DoubleMatrix v1 = c1.getRow(i);
 			
-			pw.print(v1.toString().substring(1, v1.toString().length() - 1).replaceAll(";", ",") + ",");
-			pw.print(v2.toString().substring(1, v2.toString().length() - 1).replaceAll(";", ",") + ",");
+			pw.print(v1.toString().substring(1, v1.toString().length() - 1).replaceAll(";", ",") + ", ");
+			pw.print(v2.toString().substring(1, v2.toString().length() - 1).replaceAll(";", ",") + ", ");
 			pw.println("POS");
 			
 			// create k negative examples
@@ -79,8 +207,8 @@ public class InstanceMatching {
 					j = (int) (Math.random() * c1.rows);
 				} while(i == j);
 				DoubleMatrix v1n = c1.getRow(j);
-				pw.print(v1n.toString().substring(1, v1n.toString().length() - 1).replaceAll(";", ",") + ",");
-				pw.print(v2.toString().substring(1, v2.toString().length() - 1).replaceAll(";", ",") + ",");
+				pw.print(v1n.toString().substring(1, v1n.toString().length() - 1).replaceAll(";", ",") + ", ");
+				pw.print(v2.toString().substring(1, v2.toString().length() - 1).replaceAll(";", ",") + ", ");
 				pw.println("NEG");
 			}
 			
@@ -155,7 +283,6 @@ public class InstanceMatching {
 		System.out.println("Output file: "+file);
 		
 	}
-
 	
 	private static String sameEntityOf(String string) {
 		string = string.replaceFirst("person1", "person2");
@@ -163,7 +290,6 @@ public class InstanceMatching {
 			string = sameEntityOfType(string, type);
 		return string; 
 	}
-
 
 	private static String sameEntityOfType(String string, String type) {
 		if(string.contains(type)) {
@@ -174,10 +300,9 @@ public class InstanceMatching {
 		return string;
 	}
 
-
 	private static ArrayList<String> loadLabels(String dir) throws FileNotFoundException {
 		ArrayList<String> labels = new ArrayList<>();
-		Scanner in = new Scanner(new File("etc/"+dir+"/labels_out.txt"));
+		Scanner in = new Scanner(new File("etc/"+dir+"/labels_D.txt"));
 		while(in.hasNextLine()) {
 			String line = in.nextLine();
 			labels.add(line);
